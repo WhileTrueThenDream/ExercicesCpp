daf372da680f41e40f583ecc6daff218495644dc
amaia <whiletruethendream@gmail.com>
2021-09-26 11:04:25 +0200

    alignment changed

--- Chapter_4/Exercise_4_1.cpp
+++ Chapter_4/Exercise_4_1.cpp
@@ -9,28 +9,29 @@
  * \author            amaia
  */
  
- #include <ctime>
- #include <cstdio>
+#include <ctime>
+#include <cstdio>
  
- struct TimerClass
- {
-	 TimerClass(void)
-	 {
-		 timestamp =  std::time(0);  /* save current time*/
-	 }
-	 
-	 void PrintCreationTime(void)
-	 {
-		 printf("timestamp in saved format:    %ld \n", timestamp);
-		 printf("timestamp in readable format: %s \n",std::asctime(std::localtime(&(this->timestamp)))); /* print it in a human readable format*/
-	 }
-	 
-	 private:
-	 time_t timestamp;              /* timestamp of objects creation time */
- };
+struct TimerClass
+{
+    
+    TimerClass(void)
+    {
+        timestamp =  std::time(0);  /* save current time*/
+    }
+    
+    void PrintCreationTime(void)
+    {
+        printf("timestamp in saved format:    %ld \n", timestamp);
+        printf("timestamp in readable format: %s \n",std::asctime(std::localtime(&(this->timestamp)))); /* print it in a human readable format*/
+    }
+     
+    private:
+    time_t timestamp;              /* timestamp of objects creation time */
+};
  
- int main(void)
- {
-	 TimerClass myTimer;           /* object myTimer is created */
-	 myTimer.PrintCreationTime();  /* print time of object creation */
- }
+int main(void)
+{
+    TimerClass myTimer;           /* object myTimer is created */
+    myTimer.PrintCreationTime();  /* print time of object creation */
+}
