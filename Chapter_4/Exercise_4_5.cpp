4a01b8ea3dfd276bd6b1b05bdc6ce2c87ac56a72
amaia <whiletruethendream@gmail.com>
2021-09-30 12:04:09 +0200

    alignment

--- Chapter_4/Exercise_4_5.cpp
+++ Chapter_4/Exercise_4_5.cpp
@@ -21,16 +21,16 @@ struct TimerClass
     TimerClass(const char* name)
     {
         gettimeofday(&s_timestamp,0);  
-        strncpy(&timername[0], name, (sizeof(timername)-1));			 			
+        strncpy(&timername[0], name, (sizeof(timername)-1));                         
     }
 
     TimerClass(const TimerClass& other)              /* Copy constructor */
     : s_timestamp {other.s_timestamp}
-	  
+      
     {    
         strncpy(&timername[0], other.timername, (sizeof(timername)-1));   
     }
-	
+    
     TimerClass& operator=(const TimerClass& other)   /* Copy assignment operator */
     {
         if(this == &other) 
@@ -42,15 +42,15 @@ struct TimerClass
             strncpy(&timername[0], other.timername, (sizeof(timername)-1)); 
             this->s_timestamp = other.s_timestamp;
         }
-		   		 
+                    
         return *this;                
     }
 
     TimerClass(const TimerClass&& other)               /* Move constructor */
     : s_timestamp {other.s_timestamp},
-	  moved_from{true}
+      moved_from{true}
     {
-    	strncpy(&timername[0], other.timername, (sizeof(timername)-1));
+        strncpy(&timername[0], other.timername, (sizeof(timername)-1));
     }   
     
     TimerClass& operator=(const TimerClass&& other)    /* Move assignment operator*/
@@ -61,14 +61,14 @@ struct TimerClass
         }
         else
         {
-	        this->moved_from  = true;			
+            this->moved_from  = true;            
             this->s_timestamp   = other.s_timestamp;
-	        strncpy(&timername[0], timername, (sizeof(timername)-1));
+            strncpy(&timername[0], timername, (sizeof(timername)-1));
         }
-		
+        
         return *this;                
     }
-	
+    
     ~TimerClass(void)
     {
         timeval s_endtime, s_dif; 
@@ -77,21 +77,21 @@ struct TimerClass
         timersub(&s_endtime ,&s_timestamp , &s_dif);
         
         printf("Timer: %s Init: %lds; %ldms;\t End: %lds; %ldms;\n",&timername[0], s_timestamp.tv_sec, s_timestamp.tv_usec,s_endtime.tv_sec,s_endtime.tv_usec);
-	    printf("       >> diff: %lds; %ldms;\n", s_dif.tv_sec, s_dif.tv_usec);  	
+        printf("       >> diff: %lds; %ldms;\n", s_dif.tv_sec, s_dif.tv_usec);      
         if(moved_from) printf("This timer was moved-from !!! \n\n");
     }
          
     private:
         timeval s_timestamp;           /* struct timeval with two fields, seconds and us */
-	    char timername[10];            /* timers name, 9 chars length max. */
-	    bool moved_from = false;       /* true if you were moved from, false if not */
+        char timername[10];            /* timers name, 9 chars length max. */
+        bool moved_from = false;       /* true if you were moved from, false if not */
 };
      
 int main(void)
 {
     TimerClass Timer_1{"Timer_1"};  
-    TimerClass Timer_2{Timer_1}; 	
+    TimerClass Timer_2{Timer_1};     
     TimerClass Timer_3{std::move(Timer_2)}; 
 
-    return 0;	
+    return 0;    
 }
